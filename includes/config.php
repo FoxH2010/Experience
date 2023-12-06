e684af14e634a92e9cd83b759a98b2f78be6d129
FoxH Gaming <130901846+FoxH2010@users.noreply.github.com>
2023-12-06 16:56:44 +0700

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+    define("DB_NAME", 'if0_35470039_summer_journey');
+    define("DB_USER", 'if0_35470039');
+    define("DB_PASSWORD", '47Vz9MWD3S');
+    define("DB_HOST", 'sql104.infinityfree.com');
+    define("DB_PORT", '3306');
+?>
